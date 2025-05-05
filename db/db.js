1465252d77bfa062b17b167fca37198c4ebb98b5
darez <deviansky345@gmail.com>
2025-05-05 14:23:53 +0700

    pengujian

--- db/db.js
+++ db/db.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'root',
+  password: process.env.DB_PASSWORD || '',
   database: process.env.DB_NAME || 'my_app_db'
 };
 
