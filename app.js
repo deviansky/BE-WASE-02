079ada5a9aaee5d2f7bfb229f22d97de8100382a
darez <deviansky345@gmail.com>
2025-05-20 23:10:00 +0700

    middleware umum 01

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT;
 
 // Middleware
 app.use(cors({
-  origin: '*',
+  origin: 'http://localhost:5173/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
